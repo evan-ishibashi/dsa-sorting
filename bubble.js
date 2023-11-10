1d7f257940a21e02b45ad9c3213e480fd78ec7f8
Christopher Alley <calleyviola@gmail.com>
2023-11-10 14:16:46 -0500

    Add bubble sort and start merge

--- bubble.js
+++ bubble.js
@@ -1,3 +1,24 @@
-function bubbleSort() {}
+/* Sorts an array */
+function bubbleSort(arr) {
+  let sorted = arr.slice(0);
+  let swapped = false;
+
+  // [3,1,4,5]
+
+  for (let i = arr.length; i > 1; i--) {
+    for (let j = 1; j < i; j++) {
+      if (sorted[j] < sorted[j-1]) {
+        let temp = sorted[j];
+        sorted[j] = sorted[j-1];
+        sorted[j-1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) return sorted;
+    swapped = false;
+  }
+
+  return sorted;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,4 +1,20 @@
-function merge() {}
+/** Given two sorted arrays, return an array containing the values of both
+   inputs in sorted order */
+function merge(arr1, arr2) {
+  let output = [];
+  let arr1pointer = 0;
+  let arr2pointer = 0;
+
+  while (arr1pointer < arr1.length && arr2pointer < arr2.length) {
+    if (arr1[arr1pointer] <= arr2[arr2pointer]) {
+      output.push(arr1[arr1pointer]);
+      arr1pointer++;
+    } else {
+      output.push(arr2[arr2pointer]);
+      arr2pointer;
+    }
+  }
+}
 
 function mergeSort() {}
 
